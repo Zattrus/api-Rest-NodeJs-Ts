f1802618143928fe9476a241285793d51949b066
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2024-11-28 17:27:16 -0300

    feat: add validation for cidade creation using yup V1

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,12 +1,24 @@
 import { Request, Response } from 'express';
+import * as yup from 'yup';
 
 interface ICidade {
   name: string;
 }
 
-export const create = (req: Request<{}, {}, ICidade>, res: Response) => {
-  const data = req.body.name;
-  
+const bodyValidation: yup.ObjectSchema<ICidade> = yup.object().shape({
+  name: yup.string().required().min(3),
+});
+
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  let validateData: ICidade | undefined = undefined;
+
+  try {
+    validateData = await bodyValidation.validate(req.body);
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    
+    return res.json({ errors: {default: yupError.message} });
+  }  
 
   return res.send('Create!');
 };

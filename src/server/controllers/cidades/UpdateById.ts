fe58113c7dd79ab84f8319d53bd3336f39e7ef31
Gabriel <oliveiragabriel1712@gmail.com>
2025-01-02 15:00:51 -0300

    feat: implement CRUD operations for cidades; add validation and routes for getById, updateById and deleteById

---
+++ src/server/controllers/cidades/UpdateById.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+
+import { validation } from '../../shared/middlewares';
+
+interface IParamProps {
+  id?: number;
+}
+
+
+export const updateByIdValidation = validation((getSchema) => ({
+  params: getSchema<IParamProps>(yup.object().shape({
+    id: yup.number().integer().required().moreThan(0),
+  })),
+}));
+
+export const updateById = async (req: Request<IParamProps>, res: Response) => {
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send('Não implementado!');
+};
